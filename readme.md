55d97b3ad710adea7bfd5f268cc06b1823689170
Valentin Fadeev <valentinfs@yandex.ru>
2021-12-01 22:03:02 +0300

    Tests and readme.md changed

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-[![Build status](https://ci.appveyor.com/api/projects/status/i9lilyiy8fvanvyb?svg=true)](https://ci.appveyor.com/project/ValentinFS/ajava-2-2-1)
+[![Build status](https://ci.appveyor.com/api/projects/status/i9lilyiy8fvanvyb?svg=true)](https://ci.appveyor.com/project/ValentinFS/ajava-2-3-1)
--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -15,7 +16,7 @@ public class AppCardDeliveryTest {
 
     @BeforeEach
     void setUp() {
-//        Configuration.headless = true;
+        Configuration.headless = true;
         open("http://localhost:9999");
     }
 
